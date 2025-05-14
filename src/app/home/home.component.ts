d4bc4e68ff54ee748e127065e8c30d4be73d0a07
macronnie <macr0nnieasmr@gmail.com>
2025-05-14 15:50:44 -0400

    name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  //define it
+  squareStatus = {
+    filled: false,
+    color: 'blue',
+    id: 1
+  };
+  //loop for squares
+  //squares :number = 0;
+  total_filled_squares: number = 0;
+  total_unfilled_squares: number = 0;
+  //need an object to store if the square is filled or not
+  RenderSquares() {
+    //map the squares to the dom
+    //dom element
+  }
+  MarkSquare(id: number) {
+    //check if the square is filled or not
+    if (this.squareStatus.filled) {
+      this.squareStatus.filled = false;
+      this.total_filled_squares--;
+      this.total_unfilled_squares++;
+    } else {
+      this.squareStatus.filled = true;
+      this.total_filled_squares++;
+      this.total_unfilled_squares--;
+    }
+  }
 }
